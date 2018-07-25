e287fbb5a3c4d1c3a5dc14ad6d575684713adf10
cjoannes@tulane.edu <cjoannes@tulane.edu>
2018-07-25 15:41:41 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,2 +1,3 @@
 var playlist = { beyonce: "partition" }
 function updatePlaylist(playlist, name, song) { playlist.adele = 'summer'; return Object.assign( {}, playlist, { [adele]: 'summer'}) }
+function removeFromPlaylist(obj, name) { delete.name 
